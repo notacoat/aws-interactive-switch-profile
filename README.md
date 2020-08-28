4915cae2d015dd17e82d919679a9dd11bc09a2ef
Emily Barbour <emily@boostinsurance.io>
2020-08-28 16:33:49 -0400

    v1.0.0

---
+++ README.md
@@ -0,0 +1,7 @@
+## aws-interactive-switch-profile
+
+Utilizes `aws configure list-profiles` to allow switching of profiles.
+
+Once a profile is selected, `export AWS_PROFILE=<SELECTED_PROFILE>` is run
+
+
---
+++ index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const prompts = require('prompts');
+const shell = require('shelljs');
+
+async function getProfiles() {
+  const { stdout: profiles } = await exec('aws configure list-profiles');
+  const profile_array = profiles
+    .trim()
+    .split(/\s+/)
+    .map(value => {
+    return { value }
+});
+  return profile_array;
+}
+
+async function run() {
+  const choices = await getProfiles();
+  const { profile } = await prompts({
+    type: 'select',
+    name: 'profile',
+    message: 'Select an AWS profile to switch to',
+    choices,
+  });
+
+  await writeProfile(profile);
+}
+
+async function writeProfile(profile) {
+  if (!profile) {
+    return;
+  }
+
+  shell.sed('-i', /export AWS_PROFILE=".*"/, 'export AWS_PROFILE="' + profile + '"', '~/.zshrc')
+}
+
+function onError(e) {
+  console.error(e);
+}
+
+run().catch(onError);
