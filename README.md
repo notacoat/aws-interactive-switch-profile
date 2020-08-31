d64d7f3979a071c53d16dec2ab0d4266d970d7d0
Emily Barbour <emily@boostinsurance.io>
2020-08-31 17:30:28 -0400

    v1.1.0

--- README.md
+++ README.md
@@ -1,7 +1,45 @@
 ## aws-interactive-switch-profile
 
-Utilizes `aws configure list-profiles` to allow switching of profiles.
+Utilizes `aws configure list-profiles` via aws-cli to allow the switching of profiles via [prompts](https://github.com/terkelg/prompts).  No more remembering what your AWS profiles are called.  Run the program via a helper shell function, select the profile you want to use and that's it.
 
-Once a profile is selected, `export AWS_PROFILE=<SELECTED_PROFILE>` is run
+
+### Install
+
+> $ npm install -g aws-interactive-switch-profile
+
+### Usage
+
+`aws-interactive-switch-profile '/tmp/foo.txt'`
+
+Because you cannot persist global environment variables outside of Node, you must use some sort of helper function to facilitate this.
+
+> example zsh script
+```
+aisp() {
+    unset _PROFILE;
+    # creates tmp file
+    tf=$(mktemp /tmp/aisp.XXXXXXXXX)
+    # runs aws-interactive-switch-profile and stores user selection to tmp file
+    node aws-interactive-switch-profile $tf
+    # reads in tmp file and stores to variable
+    _PROFILE=$(<$tf);
+    if [ -z $_PROFILE ];
+      then
+        echo "AWS_PROFILE not selected.";
+      else
+        export AWS_PROFILE=$_PROFILE
+    fi
+
+    rm $tf;
+}
+```
+
+### Requirements
+
+- AWS CLI
+- Node ^8.17
+
+### License
+MIT
 
 
--- index.js
+++ index.js
@@ -1,21 +1,51 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const prompts = require('prompts');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-const prompts = require('prompts');
+
+
+async function requirementsCheck() {
+  try {
+    const fileName = process.argv[2];
+    if (!fs.existsSync(fileName)) {
+      console.error('The tmp file does not exist.');
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  await exec('aws --version', (error, _, stderr) => {
+    if (error) {
+      console.error('Please install AWS CLI.');
+    } else if (stderr) {
+      console.error('stderr');
+    }
+  });
+}
 
 async function getProfiles() {
   const { stdout: profiles } = await exec('aws configure list-profiles');
-  const profile_array = profiles
+  return profiles
     .trim()
     .split(/\s+/)
-    .map(value => {
-    return { value }
-});
-  return profile_array;
+    .map((value) => {
+      return { value }
+    });
+}
+
+async function writeFile(profile) {
+  tmpFile = process.argv[2];
+  try {
+    fs.writeFileSync(tmpFile, profile);
+  } catch(e) {
+    console.error(e);
+  }
 }
 
 async function run() {
+  await requirementsCheck();
   const choices = await getProfiles();
   const { profile } = await prompts({
     type: 'select',
@@ -24,19 +54,16 @@ async function run() {
     choices,
   });
 
-  await writeProfile(profile);
-}
-
-async function writeProfile(profile) {
-  if (!profile) {
-    return;
-  }
-
-  exec(`sed -i"b" 's/export AWS_PROFILE=".*"/export AWS_PROFILE="${profile}"/' ~/.zshrc`)
+  await writeFile(profile);
 }
 
 function onError(e) {
-  console.error(e);
+  if (e.stderr) {
+    process.stderr.write(e.stderr)
+  } else {
+    console.error(e)
+  }
+  process.exit(1);
 }
 
 run().catch(onError);
